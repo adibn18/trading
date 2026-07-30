24af8d6c73bba329e97e2448da10aee5097acaed
Adib Nawaz <adibnawaz18@gmail.com>
2026-07-30 08:23:02 +0000

    Socket established

--- src/client_session.cpp
+++ src/client_session.cpp
@@ -8,16 +8,16 @@ ClientSession::ClientSession(boost::asio::ip::tcp::socket socket)
 
 void ClientSession::start() {
     std::cout<<"Session started with client: " << socket_.remote_endpoint() << "\n";
-    char buffer[1024];
-    boost::system::error_code erc;
-    std::size_t bytes = socket_.read_some(boost::asio::buffer(buffer), erc);
-    if(!erc){
+    while(true){
+        char buffer[1024];
+        boost::system::error_code erc;
+        std::size_t bytes = socket_.read_some(boost::asio::buffer(buffer), erc);
+        if(erc) break;
         std::string msg(buffer,bytes);
         std::cout<<"Received: " << msg << "\n";
         std::string response = "ACK\n ";
         boost::asio::write(socket_, boost::asio::buffer(response), erc);
-        if(erc) {
-            std::cout << "Write failed: " << erc.message() << "\n";
-        }
+        if(erc) break;
     }
+    std::cout<<"Client disconnected: \n";
 }
